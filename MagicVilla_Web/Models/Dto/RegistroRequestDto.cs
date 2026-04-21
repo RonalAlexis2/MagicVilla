27db55cb3a8b814ea2a979e8eb9c1a9ad16d374d
RonalAlexis2 <ronal.316@hotmail.com>
2026-04-21 14:55:15 -0500

    .Net identity v2

--- MagicVilla_Web/Models/Dto/RegistroRequestDto.cs
+++ MagicVilla_Web/Models/Dto/RegistroRequestDto.cs
@@ -4,7 +4,7 @@
     {
         public string UserName { get; set; }
 
-        public string Nombre { get; set; }
+        public string Nombres { get; set; }
 
         public string Password { get; set; }
 
